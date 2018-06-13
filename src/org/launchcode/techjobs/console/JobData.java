b6acc016d1ee6ee8f1c8c21d2bcacc4557354906
Andrea Scharf <scharfa@wustl.edu>
2018-06-13 18:10:58 -0500

    Implemented Value search without column

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -57,12 +58,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -125,4 +126,44 @@ public class JobData {
         }
     }
 
+
+    /**
+     * Search all columns
+     * Returns results of search the jobs data by key/value, using
+     * inclusion of the search term.
+     * <p>
+     * For example, searching for employer "Enterprise" will include results
+     * with "Enterprise Holdings, Inc".
+     *
+     * @param value Value of teh field to search for
+     * @return List of all jobs matching the criteria
+     */
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+
+        // load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        HashMap<String, String> searchJobs = new HashMap<>();
+        String currentJob;
+        String currentJobLower;
+        String valueLower;
+
+        for (HashMap<String, String> jobsListed : allJobs) {
+            for (Map.Entry<String, String> jobDetails : jobsListed.entrySet()) {
+                currentJob = jobDetails.getValue();
+                currentJobLower = currentJob.toLowerCase();
+                valueLower = value.toLowerCase();
+
+                if (currentJobLower.contains(valueLower)) {
+                    if (jobs.contains(jobsListed) == false){
+                        jobs.add(jobsListed);
+                    }
+                }
+
+            }
+        }
+
+        return jobs;
+    }
 }
